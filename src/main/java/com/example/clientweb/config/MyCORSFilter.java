c4751d184630f269561341e7ee97a8911a3d54a4
stimur1709 <stimur1709@mail.ru>
2022-10-16 16:23:20 +0700

    change corsFilter

--- src/main/java/com/example/clientweb/config/MyCORSFilter.java
+++ src/main/java/com/example/clientweb/config/MyCORSFilter.java
@@ -20,11 +20,11 @@ public class MyCORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("*"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, x-location");
 
         chain.doFilter(req, res);
     }
