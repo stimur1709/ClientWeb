8ec4b4563a5710806173df73251fd6f17325d649
stimur1709 <stimur1709@mail.ru>
2022-10-16 23:11:04 +0700

    fixed conflict

--- src/main/java/com/example/clientweb/config/MyCORSFilter.java
+++ src/main/java/com/example/clientweb/config/MyCORSFilter.java
@@ -3,6 +3,7 @@ package com.example.clientweb.config;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -23,11 +25,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class MyCORSFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
 
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        System.out.println("WebConfig; "+request.getRequestURI());
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
@@ -35,15 +36,14 @@ public class MyCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.addHeader("Access-Control-Expose-Headers", "responseType");
         response.addHeader("Access-Control-Expose-Headers", "observe");
-        System.out.println("Request Method: "+request.getMethod());
+
         if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
             try {
                 chain.doFilter(req, res);
-            } catch(Exception e) {
-                e.printStackTrace();
+            } catch (IOException | ServletException e) {
+                throw new RuntimeException(e);
             }
         } else {
-            System.out.println("Pre-flight");
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
             response.setHeader("Access-Control-Max-Age", "3600");
