2b2ba0e8e9032017f39315f2c169005a642b8de9
stimur1709 <stimur1709@mail.ru>
2023-04-22 13:06:04 +0700

    Revert "delete async"
    This reverts commit 5cbb8fe323fe5b7bf305bcbc716fc6ad923af5b7.

--- src/main/java/com/example/clientweb/service/impl/UserRatingsService.java
+++ src/main/java/com/example/clientweb/service/impl/UserRatingsService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+
 public class UserRatingsService extends ModelServiceImpl<UserRatings, Dto, UserRatingsRepository> {
 
     @Autowired
